d095c12aa1a238a3609b15e5da5ae0ffdc2104b4
Moises Alves <moisesrangelfilho@gmail.com>
2021-06-10 16:30:59 -0300

    esse commit altera Dia2.0(atualizacoes em Html e Css) e cria o projeto dia3.0

---
+++ Dia3.0/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stiles.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<styles />,document.getElementById('root'));
+
