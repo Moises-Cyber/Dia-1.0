2e84d2c0220d7ade90b07dd2be92d32b7243b124
Moises Alves <moisesrangelfilho@gmail.com>
2021-06-10 21:01:25 -0300

    esse commit cria um botao

---
+++ Dia3.1/src/componentes/corpitcho/indexlogo.jsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+function LogoCard(){
+    return(
+        <div>
+            <h1 className="LogoName">Serra Junior Engenharia</h1>
+            <img scr="../../../logo.png" className="sjLogo" alt="logo" />
+        </div>
+    );
+}
+
+export default LogoCard;
